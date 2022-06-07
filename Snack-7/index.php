6ab3fde1e099f3720731cb8b22f9cd871c9233cf
Amanjit313 <amanjitsingh313@gmail.com>
2022-06-07 15:03:05 +0200

    Snack 7

--- Snack-7/index.php
+++ Snack-7/index.php
@@ -2,8 +2,34 @@
 
 /* Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno. */
 
-?>
+  $alunnoUno = (
+    [
+      $nome = "Sara",
+      $cognome = "Alitico",
+      $voti = array(10, 8, 10, 6, 5),
+      $mediaVoti = array_sum($voti) / count($voti)
+      ]
+    );
 
+  $alunnoDue = (
+    [
+      $nome = "Kevin",
+      $cognome = "Katarri",
+      $voti = array(6, 8, 9, 7, 10),
+      $mediaVoti = array_sum($voti) / count($voti)
+  ]
+  );
+
+  $alunnoTre = (
+    [
+      $nome = "Omar",
+      $cognome = "Polito",
+      $voti = array(6, 6, 9, 6, 8),
+      $mediaVoti = array_sum($voti) / count($voti)
+  ]
+  );
+
+?>
 
 
 <!DOCTYPE html>
@@ -15,6 +41,22 @@
   <title>Snack 7 PHP</title>
 </head>
 <body>
+
+  <h1>Nome alunno/a = <?php echo $alunnoUno[0]?> </h1>
+  <h1>Cognome alunno/a = <?php echo $alunnoUno[1]?> </h1>
+  <h1>Media dei voti = <?php echo $alunnoUno[3]?> </h1>
+
+  <br>
+
+  <h1>Nome alunno/a = <?php echo $alunnoDue[0]?> </h1>
+  <h1>Cognome alunno/a = <?php echo $alunnoDue[1]?> </h1>
+  <h1>Media dei voti = <?php echo $alunnoDue[3]?> </h1>
+
+  <br>
+
+  <h1>Nome alunno/a = <?php echo $alunnoTre[0]?> </h1>
+  <h1>Cognome alunno/a = <?php echo $alunnoTre[1]?> </h1>
+  <h1>Media dei voti = <?php echo $alunnoTre[3]?> </h1>
 
 </body>
 </html>
