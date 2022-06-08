09b011cbb13d053c5a2f4241944d8416fbc68e7c
Amanjit313 <amanjitsingh313@gmail.com>
2022-06-08 12:42:23 +0200

    Snack 3 - Complete

---
+++ Snack-3/index.php
@@ -0,0 +1,78 @@
+<?php 
+
+/* Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. */
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+/* var_dump($posts) */
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 3 PHP</title>
+</head>
+<body>
+
+<?php foreach($posts as $data => $infos): ?>
+  <ul>
+    <li><?php echo $data ?>
+
+      <?php foreach($infos as $text): ?>
+      <ul>
+        <li><?php echo "Titolo:" ." ". $text["title"]?></li>
+        <li><?php echo "Autore:" ." ". $text["author"]?></li>
+        <li><?php echo "Testo:" ." ". $text["text"]."<br>"."<br>"?></li>
+      </ul>
+      <?php endforeach; ?>
+      
+    </li>
+  </ul>
+<?php endforeach; ?>
+
+</body>
+
+</html>
